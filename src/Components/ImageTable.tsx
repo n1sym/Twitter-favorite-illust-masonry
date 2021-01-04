b14bdfaecdc3c844a6f37e675a9ed5b5fc7626ed
hukurouo <owlbook248@gmail.com>
2021-01-05 00:59:08 +0900

    UI微修正

--- src/Components/ImageTable.tsx
+++ src/Components/ImageTable.tsx
@@ -18,91 +18,107 @@ class ImageTable extends React.Component<{}, typeImageTableState> {
     super(props);
     this.state = {
       raneItems: [],
-      loading: '',
-      screen_name: '',
-      max_id: ''
+      loading: "",
+      screen_name: "",
+      max_id: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event: { target: {value: string} }) {
-    this.setState({screen_name: event.target.value});
+  handleChange(event: { target: { value: string } }) {
+    this.setState({ screen_name: event.target.value });
   }
 
   handleSubmit(event: any) {
-    this.setState({loading: 'loading...', screen_name: this.state.screen_name.replace("@","").replace(" ","")});
-    console.log(this.state)
-    this.getiine()
+    this.setState({
+      loading: "loading...",
+      screen_name: this.state.screen_name.replace("@", "").replace(" ", ""),
+    });
+    console.log(this.state);
+    this.getiine();
     event.preventDefault();
   }
 
-  async componentDidMount () {
-    let queue: NodeJS.Timeout
-    window.addEventListener('resize', () => {
+  async componentDidMount() {
+    let queue: NodeJS.Timeout;
+    window.addEventListener("resize", () => {
       clearTimeout(queue);
-      queue = setTimeout(()=>{
-        const innerWidth = window.innerWidth
-        const windowWidth = (innerWidth>500) ? Math.floor(innerWidth/300) : 2
-        if (!!items.url){this.setState({raneItems: createRaneItems(windowWidth)})}
-      },500)
-    })
-    let scqueue: NodeJS.Timeout
-    window.addEventListener('scroll', () => {
-      clearTimeout(scqueue);
-      scqueue = setTimeout(()=>{
-        const scroll_Y = document.documentElement.scrollTop + window.innerHeight
-        const offsetHeight = document.documentElement.offsetHeight
-        if (offsetHeight - scroll_Y <= 200 && this.state.loading !== 'loading...' && offsetHeight > 1500){
-          this.setState({loading: 'loading...'})
-          this.getiine()
+      queue = setTimeout(() => {
+        const innerWidth = window.innerWidth;
+        const windowWidth = innerWidth > 500 ? Math.floor(innerWidth / 300) : 2;
+        if (!!items.url) {
+          this.setState({ raneItems: createRaneItems(windowWidth) });
         }
-      },500)
-    })
+      }, 500);
+    });
+    let scqueue: NodeJS.Timeout;
+    window.addEventListener("scroll", () => {
+      clearTimeout(scqueue);
+      scqueue = setTimeout(() => {
+        const scroll_Y =
+          document.documentElement.scrollTop + window.innerHeight;
+        const offsetHeight = document.documentElement.offsetHeight;
+        if (
+          offsetHeight - scroll_Y <= 500 &&
+          this.state.loading !== "loading..." &&
+          offsetHeight > 1500
+        ) {
+          this.setState({ loading: "loading..." });
+          this.getiine();
+        }
+      }, 500);
+    });
   }
-  getiine = async() => {
-    const images = await apitest(this.state.screen_name, this.state.max_id)
-    console.log(images)
+  getiine = async () => {
+    const images = await apitest(this.state.screen_name, this.state.max_id);
+    console.log(images);
     if (items.url) {
-      items.url = items.url.concat(images.url)
-      items.height = items.height.concat(images.height)
-      items.source = items.source.concat(images.source)
+      items.url = items.url.concat(images.url);
+      items.height = items.height.concat(images.height);
+      items.source = items.source.concat(images.source);
     } else {
-      items = images
+      items = images;
     }
-    const innerWidth = window.innerWidth
-    const windowWidth = (innerWidth>500) ? Math.floor(innerWidth/300) : 2
-    this.setState({raneItems: createRaneItems(windowWidth), max_id: images.max_id, loading: ''})
-  }
+    const innerWidth = window.innerWidth;
+    const windowWidth = innerWidth > 500 ? Math.floor(innerWidth / 300) : 2;
+    this.setState({
+      raneItems: createRaneItems(windowWidth),
+      max_id: images.max_id,
+      loading: "",
+    });
+  };
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-        <div className="flex justify-center mb-5 mx-5">
-          <p>twitterのスクリーンネームを入力してください (例：@hukurouo) </p>
-        </div>
-        <div className="flex mb-5 mx-auto max-w-xs">
-  <div className="mt-1 relative rounded-md shadow-sm mx-5 w-full">
-    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-      <span className="text-gray-500 sm:text-sm">
-        @
-      </span>
-    </div>
-    <input type="text"
-                 autoCapitalize="off"
-                 name="screen_name"
-                 placeholder="hukurouo" 
-                 value={this.state.screen_name} 
-                 onChange={this.handleChange} 
-                 className="bg-white focus:outline-none focus:shadow-outline py-2 px-8 rounded-md w-full"
+          <div className="flex justify-center mb-5 mx-5">
+            <p>twitterのスクリーンネームを入力してください（例：@hukurouo）</p>
+          </div>
+          <div className="flex mb-5 mx-auto max-w-xs">
+            <div className="mt-1 relative rounded-md shadow-sm mx-5 w-full">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <span className="text-gray-500 sm:text-sm">@</span>
+              </div>
+              <input
+                type="text"
+                autoCapitalize="off"
+                name="screen_name"
+                value={this.state.screen_name}
+                onChange={this.handleChange}
+                className="bg-white focus:outline-none focus:shadow-outline py-2 px-8 rounded-md w-full"
+              />
+            </div>
+          </div>
+          <input
+            type="submit"
+            value="取得"
+            disabled={this.state.screen_name === ""}
+            className="flex justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded w-20 mx-auto mb-10"
           />
-          
-  </div>
-</div>
-        <input type="submit" value="取得" disabled={this.state.screen_name === ''} className="flex justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded w-20 mx-auto" />
         </form>
 
-        <ImageList raneItems={this.state.raneItems}/>
+        <ImageList raneItems={this.state.raneItems} />
         <div className="box h-64 text-center m-5 p-4 ...">
           {this.state.loading}
         </div>
