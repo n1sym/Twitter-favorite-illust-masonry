ac18fb540d0ad617505a0581d8659b9249df52ca
hukurouo <owlbook248@gmail.com>
2021-01-10 12:18:10 +0900

    bg

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <Helmet>
         <title>iineum</title>
       </Helmet>
-      <div className="bg-blue-50 min-h-screen">
+      <div className="bg-indigo-100 min-h-screen">
         <div className="container mx-auto">
           <header className="flex justify-center items-center text-3xl h-32 mx-5">
             いいねした画像を並べるサイト
